f7a839ab9dbe556a4bf1ec46a7e51018d91e96bc
Daniel Dyla <dyladan@gmail.com>
2015-04-21 13:52:23 -0400

    Added tests for POST submission of forms

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,23 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from whatistheplan.forms import UserForm, UserProfileForm
 
 class UserFormTest(TestCase):
-    def test_user_form(self):
-        form_data = {
+    def setUp(self):
+        self.user_form_data = {
             'username': 'sn1par_eugene',
             'password': 'bloxwich',
             'email': 'h00bastankR0x@aol.com'
         }
-        form = UserForm(data=form_data)
-        self.assertEqual(form.is_valid(), True)
-
-    def test_user_profile_form(self):
-        form_data = {
+        self.user_profile_form_data = {
             'name': "Eugene M'TnDew",
             'mac_address': '00:00:00:00i:00:00',
             'steamid': 'xXx_sn1par_eugene_420_xXx',
             'irc': 'eugene'
         }
-        form = UserProfileForm(data=form_data)
+        self.form_data = self.user_form_data.copy()
+        self.form_data.update(self.user_profile_form_data)
+        self.client = Client()
+
+    def test_user_form_is_valid(self):
+        """sample data is valid for user form"""
+        form = UserForm(data=self.user_form_data)
         self.assertEqual(form.is_valid(), True)
+
+    def test_user_profile_form_is_valid(self):
+        """sample data is valid for user profile form"""
+        form = UserProfileForm(data=self.user_profile_form_data)
+        self.assertEqual(form.is_valid(), True)
+
+    def test_user_form_submission_success(self):
+        """form submission with sample data success"""
+        resp = self.client.post("/signup/", self.form_data)
+        self.assertEqual(resp.status_code, 200)
 
