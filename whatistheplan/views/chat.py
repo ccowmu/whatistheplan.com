d85282f5d8591ba02216de63cfa6f3b6df2ea85b
gigglesbw4 <blakewrege1@gmail.com>
2015-10-21 06:15:14 -0400

    Revert "merged i This reverts commit cd06e9e217f40c779ec63219e58e234213b522b6.

--- whatistheplan/views/chat.py
+++
@@ -1,8 +0,0 @@
-"""view controller for about route"""
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-
-def chat(request):
-    """Chat view"""
-    context = RequestContext(request)
-    return render_to_response('chat.html', {}, context_instance=context)
