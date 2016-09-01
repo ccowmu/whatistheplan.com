d132b6e94776a1535e81e7e5d78b830ba8727ed9
chrissphinx <chrissphinx@gmail.com>
2016-09-01 18:50:52 -0400

    Removed Sloan's email, tidied up the title syntax and render() syntax (#45)

--- whatistheplan/models/game.py
+++ whatistheplan/models/game.py
@@ -5,4 +5,12 @@ class Game(models.Model):
     """
     game_name -> Counter Strike Source
     """
-    game_name = models.CharField(max_length=256)
+    # game_name = models.CharField(max_length=256)
+    # game_time = models.CharField(max_length=256)
+    # game_room = models.CharField(max_length=256)
+    # game_1st_prize = models.Charfield(max_length=256)
+    # game_2nd_prize = models.Charfield(max_length=256)
+    # game_3rd_prize = models.Charfield(max_length=256)
+    # game_details = models.Charfield(max_length=1024)
+    # 
+    # game_image = models.ImageField()
