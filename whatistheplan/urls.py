8f9c9634d213cb271a455964b09ad059dffee679
benjamincampbell <bencampbell04@gmail.com>
2016-10-13 17:31:45 -0400

    Added robots.txt file to help (try to) only allow Google and Bing, disallow all others

--- whatistheplan/urls.py
+++ whatistheplan/urls.py
@@ -1,6 +1,7 @@
 """Define basic routes"""
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 # Import the views from views.py
 from whatistheplan import views
@@ -24,5 +25,6 @@ urlpatterns = [ # pylint: disable=invalid-name
     url(r'^signup/', views.google_signup, name='Sign Up'),
     url(r'^login/', views.user_login, name='Log In'),
     url(r'^logout/', views.user_logout, name='Log Out'),
-    url(r'^twitch/', views.twitch, name='Twitch')
+    url(r'^twitch/', views.twitch, name='Twitch'),
+    url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots_file")
 ]
